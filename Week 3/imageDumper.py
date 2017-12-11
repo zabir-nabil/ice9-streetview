46c710f9606b44526d7a4af07be1f824b3022c8a
mdmmn378 <mdmmn378@gmail.com>
2017-12-12 02:55:22 +0600

    late update

--- Week 3/imageDumper.py
+++ Week 3/imageDumper.py
@@ -1,31 +1,78 @@
 from requests import get
 from gis import KML2DICT
+from PIL import Image
+import logging
+from threading import Thread
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
 import io
-import cv2 as cv
+
 
 key = 'AIzaSyCviDo0j3-AdFzULn2T-nuOOFmW3PwfhLc'
 
-df = KML2DICT('wards.kml')
+loc='wards.kml'
 
-for name in df.getKeys():
-    print(name)
-    if not os.path.exists(name):
-        os.makedirs(name)
-    for d in df.generate(name):
-        print(d[0], d[1])
-        for heading in range(0, 360, 60):
-            gurl = "https://maps.googleapis.com/maps/api/streetview?size=640x640&location={0},{1}&fov=120&heading={2}&pitch=10&key={3}".format(d[0], d[1], heading, key)
-            r = get(gurl, stream=True)
-            mg = Image.open(io.BytesIO(r.content))
-            image = np.array(mg)
-            plt.imsave('{0}/{1}_{2}_{3}.jpg'.format(name,name,d[0],
-                                                    heading),
-                       image)
-            cv.imshow('frame', cv.cvtColor(image, cv.COLOR_BGR2RGB))
-            cv.waitKey(1)
 
-cv.destroyAllWindows()
+
+class Dump(object):
+    def __init__(self,key,loc,nop=10):
+        self.key=key
+        self.df = KML2DICT(loc)
+        self.nop=nop
+        self.threads=[]
+
+        self.logger = logging.getLogger('imageDumper')
+        hdlr = logging.FileHandler('fails.log')
+        formatter = logging.Formatter(
+            '%(asctime)s %(levelname)s %(message)s')
+        hdlr.setFormatter(formatter)
+        self.logger.addHandler(hdlr)
+        self.logger.setLevel(logging.WARNING)
+
+    def listPartition(self,alist, partition):
+        for i in range(0, len(alist), partition):
+            yield alist[i:i + partition]
+
+
+    def multiThread(self,names):
+
+        for name in names:
+            print(name)
+            if not os.path.exists(name):
+                os.makedirs(name)
+            else:
+                continue
+            for d in self.df.generate(name):
+                print(d[0], d[1])
+                for heading in range(0, 360, 60):
+                    gurl = "https://maps.googleapis.com/maps/api/streetview" \
+                           "?size=640x640&location={0},{1}&fov=120&heading={2}&pitch=10&key={3}".format(d[0], d[1], heading,
+                                                        self.key)
+                    try:
+                        r = get(gurl, stream=True)
+                        mg = Image.open(io.BytesIO(r.content))
+                        image = np.array(mg)
+                        plt.imsave('{0}/{1}_{2}_{3}.jpg'.format(name,name,d[0],heading),image)
+
+                    except:
+                        self.logger.error("<{0},{1}> < {2} >".format(
+                            d[0],d[1],name))
+                        print('Failed!')
+    def run(self):
+        keys=list(self.df.getKeys())
+        for nms in self.listPartition(keys, int(len(keys)/self.nop)):
+            self.threads.append(Thread(target=self.multiThread,kwargs={
+                'names':nms},
+                   daemon=True))
+
+        for t in self.threads:
+            t.start()
+        for t in self.threads:
+            t.join()
+
+
+a=Dump(key=key,loc=loc)
+
+a.run()
+
